3d3ab4ed6bb4f73206f9e7a206dcc1f849e864c1
Damien Pretet <damien.pretet@me.com>
2019-06-30 16:52:23 +0200

    Add random Haiku printing along coding quotes

---
+++ quotify.haiku.zsh
@@ -0,0 +1,57 @@
+# From http://www.devtopics.com/101-great-computer-programming-quotes/
+
+haiku_quotes=(
+
+"How admirable,\n\
+He who thinks not, “Life is fleeting,”\n\
+When he sees the lightning!"
+
+"The long night;\n\
+The sound of the water\n\
+Says what I think."
+
+"The stars on the pond;\n\
+Again the winter shower\n\
+Ruffles the water."
+
+"In the dark forest\n\
+A berry drops:\n\
+The sound of the water."
+
+"Sleet falling;\n\
+Fathomless, infinite\n\
+Loneliness."
+
+"Winter desolation;\n\
+In the rain-water tub,\n\
+Sparrows are walking."
+
+"The evening haze;\n\
+Thinking of past things,\n\
+How far-off they are!"
+
+"Leaves falling,\n\
+Lie on one another;\n\
+The rain beats on the rain."
+
+"The wind brings\n\
+Fallen leaves enough\n\
+To make a fire."
+
+"A trout leaps;\n\
+Clouds are moving\n\
+In the bed of the stream."
+
+"A fallen flower\n\
+Returning to the branch?\n\
+It was a butterfly."
+
+"The thief\n\
+Left it behind\n\
+The moon at the window."
+
+"This dewdrop world\n\
+It may be a dewdrop,\n\
+And yet—and yet."
+)
+
